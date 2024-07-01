8eeb21cf7eb56305ff53f788bb3757df046f24d9
Bill Harding <wpharding1@gmail.com>
2024-07-01 14:00:43 -0500

    add a static cache and use associative arrays for performance improvements
    Co-authored-by: Ishmael Smyrnow <ismyrnow@gmail.com>

--- src/DayDifference.php
+++ src/DayDifference.php
@@ -25,6 +25,8 @@ class DayDifference
 
     private DateTimeInterface $startDate;
 
+    private static array $cache = [];
+
     /**
      * @param array<int>    $allowedDaysOfTheWeek
      * @param array<string> $excludedDates
@@ -33,12 +35,22 @@ class DayDifference
         DateTimeInterface $startDate,
         DateTimeInterface $endDate,
         array $allowedDaysOfTheWeek = [0, 1, 2, 3, 4, 5, 6],
-        array $excludedDates = []
+        array $excludedDates = [],
     ) {
         $this->startDate = $startDate;
         $this->endDate = $endDate;
         $this->allowedDaysOfTheWeek = $allowedDaysOfTheWeek;
         $this->excludedDates = $excludedDates;
+    }
+
+    static function set_cache($cache)
+    {
+        self::$cache = $cache;
+    }
+
+    static function get_cache()
+    {
+        return self::$cache;
     }
 
     /**
@@ -69,6 +81,12 @@ class DayDifference
      */
     private function findDifference(int $startStamp, int $endStamp, array $allowedDaysOfTheWeek = [], array $excludedDates = []): int
     {
+        // Convert $allowedDaysOfTheWeek to a set-like associative array
+        $allowedDaysSet = array_flip($allowedDaysOfTheWeek);
+
+        // Convert $excludedDates to a set-like associative array
+        $excludedDatesSet = array_flip($excludedDates);
+
         $isFullWeek = count($allowedDaysOfTheWeek) === 7;
         $hasExclusions = !empty($excludedDates);
         $totalDays = (int) ceil(($endStamp - $startStamp) / self::SECONDS_IN_ONE_DAY);
@@ -84,20 +102,26 @@ class DayDifference
              *
              * @var string $dateFormats
              */
-            $dateFormats = date('w,Y-m-d,*-m-d', $currentStamp);
+            if (!isset(self::$cache[$currentStamp])) {
+                $dateFormats = date('w,Y-m-d,*-m-d', $currentStamp);
 
-            /**
-             * @var string $weekDay the number (in string form) representing the current day
-             * @var string $specificDate Y-m-d format of the current day
-             * @var string $wildcardDate *-m-d format of the current day, to allow for exclusions that repeat every year
-             */
-            [$weekDay, $specificDate, $wildcardDate] = explode(',', $dateFormats);
+                /**
+                 * @var string $weekDay the number (in string form) representing the current day
+                 * @var string $specificDate Y-m-d format of the current day
+                 * @var string $wildcardDate *-m-d format of the current day, to allow for exclusions that repeat every year
+                 */
+                [$weekDay, $specificDate, $wildcardDate] = explode(',', $dateFormats);
+
+                self::$cache[$currentStamp] = [$weekDay, $specificDate, $wildcardDate];
+            } else {
+                [$weekDay, $specificDate, $wildcardDate] = self::$cache[$currentStamp];
+            }
 
             // Reduce the total days if the current date in the loop is to be excluded.
             if (
-                !in_array((int) $weekDay, $allowedDaysOfTheWeek) // Day is skipped because it's not in the $allowedDaysOfTheWeek.
-                || in_array($specificDate, $excludedDates) // Day is skipped because it's a specifically excluded date.
-                || in_array($wildcardDate, $excludedDates) // Day is skipped because the month/day is excluded every year.
+                !isset($allowedDaysSet[(int) $weekDay]) // Day is skipped because it's not in the $allowedDaysSet.
+                || isset($excludedDatesSet[$specificDate]) // Day is skipped because it's a specifically excluded date.
+                || isset($excludedDatesSet[$wildcardDate]) // Day is skipped because the month/day is excluded every year.
             ) {
                 --$totalDays;
             }
